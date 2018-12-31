e53ed4ddc7045fa2b2a144be2e5be67d5c65343c
AnkiKrish <ankitabhargav3@gmail.com>
2018-12-31 17:12:09 +0530

    Sending my project

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var routes = express.Router();
 
 routes.use("/",require("../controller/home"));
-routes.use("/dashboard",require("../controller/dashboard"));
+//routes.use("/dashboard",require("../controller/dashboard"));
 routes.use("/login",require("../controller/login"));
 routes.use("/register",require("../controller/register"));
 
--- controller/login.js
+++ controller/login.js
@@ -1,36 +1,17 @@
 var express = require("express");
-var routes = express.Router();
+var routes= express.Router();
 var sha1 = require("sha1");
 var session = require("express-session");
 var user = require("../model/user");
 
 routes.get("/",function(req,res){
-    var pagedata = {title: "login Page", pagename:"login/index", message: req.flash("msg")};
+    var pagedata = {title: "login Page",pagename:"login/index"};
     res.render("layout",pagedata);
 });
-
 routes.post("/",function(req,res){
-    var u = req.body.email;
-    var p = req.body.password;
-
-    user.find({username : u},function(err,result){
-        if(err){
-          console.log("Login error in controller", err);
-          return; }
-        if(result.length > 0){
-              if(result[0].password == sha1(p)){
-                req.session._id = result[0]._id;
-                req.session.name = result[0].name;
-                req.session.is_user_logged_in = true;
-                res.redirect("/dashboard");
-            }else{
-                req.flash("msg","Password Incorrect");
-                res.redirect("/login");
-            }
-        }else {
-            req.flash("msg", "Username & Password Incorrect");
-            res.redirect("/login");
-        }
-    });
+    console.log(req.body);
 });
+
+
 module.exports = routes;
+
